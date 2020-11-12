ea8299e8c5d99345781f68982438b593a35b9119
Bernard <jordy.bernard@dfg.alaska.local>
2020-11-12 12:25:41 -0900

    dd

--- R/source_directory.R
+++ R/source_directory.R
@@ -1,8 +1,1 @@
-#' Source all files in a directory
-#' @param directory (character) the directory in which all files will be source
-#' @export
-source_directory <- function(directory="S:/ChenaSalchaSR2020/Rfunctions"){
-  for (i in 1:length(list.files(directory))){
-    source(paste(directory, "/", list.files(directory), sep="")[i])
-  }
-}
+
